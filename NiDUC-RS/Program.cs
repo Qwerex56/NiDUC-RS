6cca75785eec8cbb155c18d3d0cfb43e945be36e
Adrian Czubaty <adriancz.prywatny@gmail.com>
2024-04-28 00:48:26 +0200

    Added basic encoder

--- NiDUC-RS/Program.cs
+++ NiDUC-RS/Program.cs
@@ -1,3 +1,14 @@
-﻿using NiDUC_RS.RS_Coder;
+﻿using NiDUC_RS.GaloisField.Gf2Polynomial;
+using NiDUC_RS.RS_Coder;
 
-var reedSalomonCoder = new ReedSalomonCoder(6, 4);
+var reedSalomonCoder = new ReedSalomonCoder(3, 1);
+var msg = new Gf2Polynomial([
+                                new (0, 4),
+                                new (2, 3),
+                                new (6, 2),
+                                new (2, 1),
+                                new (6, 0)
+                            ]);
+
+var codedMsg = reedSalomonCoder.EncodeMessage(msg);
+Console.WriteLine(codedMsg);
