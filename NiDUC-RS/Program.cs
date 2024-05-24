83002e6353929964c5cf8178af8018c765f06a1e
Adrian Czubaty <adriancz.prywatny@gmail.com>
2024-05-24 18:05:48 +0200

    Changed simplified decoder

--- NiDUC-RS/Program.cs
+++ NiDUC-RS/Program.cs
@@ -1,34 +1,9 @@
-using NiDUC_RS.GaloisField.Gf2Polynomial;
-using NiDUC_RS.GaloisField.Gf2Tables;
 using NiDUC_RS.RS_Coder;
 
+var reedSalomonCoder = new ReedSolomonCoder(3, 2);
+var packets = reedSalomonCoder.SendBits("1")[0];
+const string erroredPacket = "011010001011001010011";
 
-// NiDUC_RS.GaloisField.Gf2Math.SetGf2 = new Gf2LookUpTable(3, 0b1011);
-// var poly1 = new Gf2Polynomial(new PolynomialWord(0, 1));
-// var poly2 = new Gf2Polynomial(new PolynomialWord(0, 3));
-//
-// Console.WriteLine(poly1 / poly2);
-// Console.WriteLine(poly2 / poly1);
-
-var reedSalomonCoder = new ReedSolomonCoder(6, 4);
-
-const string msg = "h";
-// const string encodedMsg = "000000001101100010"; // Error at idx = 4  0 -> 1, original string:  001101000010110
-// const string mochnacki = "001010001010001110111"; // z Mochnackiego p. 2.8.2. 
-
-var packets = reedSalomonCoder.SendBits("1");
-
-Console.WriteLine(packets[0]);
-
-foreach (var wPacket in packets.Select(packet => "1" + packet[1..^1] + "1")) {
-    var str = reedSalomonCoder.ReceiveString(wPacket);
-    Console.WriteLine(str);
-}
-
-Gf2Polynomial.FromBinaryString("1").ToBinaryString(); // 001000111 -> 0 null 5
-// Console.WriteLine(reedSalomonCoder.ReceiveString(encodedMsg));
-
-// 001111001010110 random value
-// 001101000010110
-// 001101000010110
-// 01110011
+Console.WriteLine($"Original:   {packets}");
+Console.WriteLine($"With error: {erroredPacket}");
+Console.WriteLine($"Corrected:  {reedSalomonCoder.ReceiveString(erroredPacket)}");
